3922981e4fdd52af75f1948718c3e8c157816eb9
19383273-Mayowa-Adebambo <19383273@brookes.ac.uk>
2026-04-15 16:19:49 +0100

    Global top bar date improved
    Updated the globar top bar to output a more detailed selected date range

--- src/utils/calculations.py
+++ src/utils/calculations.py
@@ -11,10 +11,10 @@ LIMITS = {
         'O3': {'8h': 120, 'annual_allowed': 10}
     },
     'WHO': {
-        'PM2.5': {'daily': 15,'annual':5},
-        'PM10': {'daily': 45,'annual':15},
-        'O3': {'8h': 100,'peak':60},
-        'NO2': {'daily': 25,'annual':10},
+        'PM2.5': {'daily': 15, 'annual': 5},
+        'PM10': {'daily': 45, 'annual': 15},
+        'O3': {'8h': 100, 'peak': 60},
+        'NO2': {'daily': 25, 'annual': 10},
         'SO2': {'daily': 40}
     }
 }
@@ -26,12 +26,14 @@ POLLUTANT_DISPLAY_NAMES = {
     'O3': 'O₃',
     'SO2': 'SO₂'
 }
-aqi_limits = {'O3':{1:33,2:66,3:100,4:120,5:140,6:160,7:187,8:213,9:240},
-              'NO2':{1:67,2:134,3:200,4:267,5:334,6:400,7:467,8:534,9:600},
-              'SO2':{1:88,2:177,3:266,4:354,5:443,6:532,7:710,8:887,9:1064},
-              'PM2.5':{1:11,2:23,3:35,4:41,5:47,6:53,7:58,8:64,9:70},
-              'PM10':{1:16,2:33,3:50,4:58,5:66,6:75,7:83,8:91,9:100}
+aqi_limits = {'O3': {1: 33, 2: 66, 3: 100, 4: 120, 5: 140, 6: 160, 7: 187, 8: 213, 9: 240},
+              'NO2': {1: 67, 2: 134, 3: 200, 4: 267, 5: 334, 6: 400, 7: 467, 8: 534, 9: 600},
+              'SO2': {1: 88, 2: 177, 3: 266, 4: 354, 5: 443, 6: 532, 7: 710, 8: 887, 9: 1064},
+              'PM2.5': {1: 11, 2: 23, 3: 35, 4: 41, 5: 47, 6: 53, 7: 58, 8: 64, 9: 70},
+              'PM10': {1: 16, 2: 33, 3: 50, 4: 58, 5: 66, 6: 75, 7: 83, 8: 91, 9: 100}
               }
+
+
 def calculate_exceedance(df, pollutant, threshold_type='UK'):
     """
     Calculate pollutant exceedances.
@@ -150,105 +152,116 @@ def calculate_exceedance(df, pollutant, threshold_type='UK'):
         'type': 'none'
     }
 
+
 def exceedance_summary(df):
-    results=[]
-    #group data by site, year and pollutant ignore missing values
-    grouped = df.dropna(subset=['value']).groupby(['site','year','pollutants'])
-    for (site,year,pollutant),wales_data in grouped:
-        wales_data=wales_data.copy()
-        wales_data['date']=pd.to_datetime(wales_data['date'])
-        #workout who value 
-        if pollutant in ['PM2.5','PM10','NO2']:
-            #for these pollutants use annual mean
+    results = []
+    # group data by site, year and pollutant ignore missing values
+    grouped = df.dropna(subset=['value']).groupby(
+        ['site', 'year', 'pollutants'])
+    for (site, year, pollutant), wales_data in grouped:
+        wales_data = wales_data.copy()
+        wales_data['date'] = pd.to_datetime(wales_data['date'])
+        # workout who value
+        if pollutant in ['PM2.5', 'PM10', 'NO2']:
+            # for these pollutants use annual mean
             value = wales_data['value'].mean()
             who_limit = LIMITS['WHO'][pollutant]['annual']
         elif pollutant == 'O3':
-            #workout the max 8hour rolling daily then workout the max rolling consecutive months
-            wales_data['date']=pd.to_datetime(wales_data['date'])
+            # workout the max 8hour rolling daily then workout the max rolling consecutive months
+            wales_data['date'] = pd.to_datetime(wales_data['date'])
             wales_data = wales_data.sort_values('date')
             wales_data = wales_data.set_index('date')
-            wales_data['8h_mean']= wales_data['value'].rolling('8h',min_periods=6).mean()
+            wales_data['8h_mean'] = wales_data['value'].rolling(
+                '8h', min_periods=6).mean()
             wales_data = wales_data.reset_index()
-            daily_max = (wales_data.groupby(wales_data['date'].dt.date)['8h_mean'].max().reset_index())
-            daily_max['date']=pd.to_datetime(daily_max['date'])
-            daily_max['month']=daily_max['date'].dt.to_period('M')
-            monthly_mean = (daily_max.groupby('month')['8h_mean'].mean().reset_index())
+            daily_max = (wales_data.groupby(wales_data['date'].dt.date)[
+                         '8h_mean'].max().reset_index())
+            daily_max['date'] = pd.to_datetime(daily_max['date'])
+            daily_max['month'] = daily_max['date'].dt.to_period('M')
+            monthly_mean = (daily_max.groupby('month')[
+                            '8h_mean'].mean().reset_index())
             monthly_mean = monthly_mean.sort_values('month')
-            monthly_mean['6m']=(monthly_mean['8h_mean'].rolling(window=6,min_periods=6).mean())
+            monthly_mean['6m'] = (monthly_mean['8h_mean'].rolling(
+                window=6, min_periods=6).mean())
             value = monthly_mean['6m'].max()
             who_limit = LIMITS['WHO']['O3']['peak']
 
         elif pollutant == 'SO2':
-            #use number of days exceeding the who limits 
-            daily_mean = wales_data.groupby(wales_data['date'].dt.date)['value'].mean()
-            value = (daily_mean>LIMITS['WHO']['SO2']['daily']).sum()
+            # use number of days exceeding the who limits
+            daily_mean = wales_data.groupby(wales_data['date'].dt.date)[
+                'value'].mean()
+            value = (daily_mean > LIMITS['WHO']['SO2']['daily']).sum()
             who_limit = 0
         who_value = value
-        #decide if who limit is exceeded 
+        # decide if who limit is exceeded
         if pollutant == 'SO2':
-            #for so2 any value above 0 means yes 
-            if who_value >0:
+            # for so2 any value above 0 means yes
+            if who_value > 0:
                 who_exceeds = 'Above'
             else:
                 who_exceeds = 'Within'
         elif pollutant == 'O3':
-            #compare to the peak limit of 6 months
+            # compare to the peak limit of 6 months
             if who_value > LIMITS['WHO']['O3']['peak']:
                 who_exceeds = 'Above'
             else:
                 who_exceeds = 'Within'
         else:
-            #for others compare to the annual limit
+            # for others compare to the annual limit
             if who_value > LIMITS['WHO'][pollutant]['annual']:
                 who_exceeds = 'Above'
             else:
                 who_exceeds = 'Within'
-        
+
         if pollutant == 'PM2.5':
-            #use annual mean
+            # use annual mean
             value = wales_data['value'].mean()
             uk_limit = LIMITS['UK']['PM2.5']['annual']
         elif pollutant == 'PM10':
-            #count days above daily limit
-            daily_mean = wales_data.groupby(wales_data['date'].dt.date)['value'].mean()
-            value = (daily_mean>50).sum()
-            uk_limit= LIMITS['UK']['PM10']['annual_allowed']
+            # count days above daily limit
+            daily_mean = wales_data.groupby(wales_data['date'].dt.date)[
+                'value'].mean()
+            value = (daily_mean > 50).sum()
+            uk_limit = LIMITS['UK']['PM10']['annual_allowed']
         elif pollutant == 'SO2':
-            #count days above daily limit
-            daily_mean = wales_data.groupby(wales_data['date'].dt.date)['value'].mean()
-            value = (daily_mean>125).sum()
+            # count days above daily limit
+            daily_mean = wales_data.groupby(wales_data['date'].dt.date)[
+                'value'].mean()
+            value = (daily_mean > 125).sum()
             uk_limit = LIMITS['UK']['SO2']['annual_allowed']
         elif pollutant == 'NO2':
-            #count hours above hourly limit
-            value = (wales_data['value']>200).sum()
+            # count hours above hourly limit
+            value = (wales_data['value'] > 200).sum()
             uk_limit = LIMITS['UK']['NO2']['annual_allowed']
-        elif pollutant == 'O3': #working out the rolling 8 hour mean then counting exceedances
-            wales_data['date']=pd.to_datetime(wales_data['date'])
+        elif pollutant == 'O3':  # working out the rolling 8 hour mean then counting exceedances
+            wales_data['date'] = pd.to_datetime(wales_data['date'])
             wales_data = wales_data.sort_values('date')
             wales_data = wales_data.set_index('date')
-            wales_data['8h_mean'] = wales_data['value'].rolling('8h', min_periods=6).mean()
+            wales_data['8h_mean'] = wales_data['value'].rolling(
+                '8h', min_periods=6).mean()
             wales_data = wales_data.reset_index()
-            daily_max = wales_data.groupby(wales_data['date'].dt.date)['8h_mean'].max()
+            daily_max = wales_data.groupby(wales_data['date'].dt.date)[
+                '8h_mean'].max()
             value = (daily_max > 120).sum()
             uk_limit = LIMITS['UK']['O3']['annual_allowed']
 
         uk_value = value
-        #check if uk limit is exceeded
+        # check if uk limit is exceeded
         if uk_value > uk_limit:
             uk_exceeds = 'Above'
         else:
             uk_exceeds = 'Within'
-    #store all results for the site, year and pollutant
+    # store all results for the site, year and pollutant
         results.append({
             'Site': site,
-            'Year':year,
+            'Year': year,
             'pollutant': pollutant,
-            'uk_value':uk_value,
-            'uk_limit':uk_limit,
-            'uk_exceeds':uk_exceeds,
-            'who_value':who_value,
-            'who_limit':who_limit,
-            'who_exceeds':who_exceeds})
+            'uk_value': uk_value,
+            'uk_limit': uk_limit,
+            'uk_exceeds': uk_exceeds,
+            'who_value': who_value,
+            'who_limit': who_limit,
+            'who_exceeds': who_exceeds})
     results_data = pd.DataFrame(results)
     results_data['Year'] = results_data['Year'].astype(int)
     results_data['Year_str'] = results_data['Year'].astype(str)
@@ -404,9 +417,9 @@ def format_date_range(start_date, end_date):
         end = pd.to_datetime(end_date)
 
         if start.year == end.year:
-            return f"{start.strftime('%b')} – {end.strftime('%b %Y')}"
+            return f"{start.strftime('%d %b')} – {end.strftime('%d %b %Y')}"
         else:
-            return f"{start.strftime('%b %Y')} – {end.strftime('%b %Y')}"
+            return f"{start.strftime('%d %b %Y')} – {end.strftime('%d %b %Y')}"
     except:
         return "--"
 
@@ -420,28 +433,32 @@ def hex_to_rgba(hex_color, alpha=0.12):
     return f"rgba({r},{g},{b},{alpha})"
 
 
-#pollution rose
-def aqi_index(value,pollutant):
+# pollution rose
+def aqi_index(value, pollutant):
     if pd.isna(value):
         return np.nan
     else:
-        for i,limit in aqi_limits[pollutant].items():
-            if value <=limit:
+        for i, limit in aqi_limits[pollutant].items():
+            if value <= limit:
                 return i
         return 10
-#assigning category to aqi
+# assigning category to aqi
+
+
 def aqi_category(index_value):
     if pd.isna(index_value):
         return np.nan
     elif index_value <= 3:
         return 'Low'
-    elif index_value <= 6 :
+    elif index_value <= 6:
         return 'Moderate'
     elif index_value <= 9:
         return 'High'
     else:
         return 'Very High'
-#changing the degrees to direction
+# changing the degrees to direction
+
+
 def degrees_to_direction(degree):
     if pd.isna(degree):
         return np.nan
@@ -449,7 +466,7 @@ def degrees_to_direction(degree):
         return 'N'
     elif degree < 67.5:
         return 'NE'
-    elif degree <112.5:
+    elif degree < 112.5:
         return 'E'
     elif degree < 157.5:
         return 'SE'
@@ -457,7 +474,7 @@ def degrees_to_direction(degree):
         return 'S'
     elif degree < 247.5:
         return 'SW'
-    elif degree <292.5:
+    elif degree < 292.5:
         return 'W'
     else:
         return 'NW'
