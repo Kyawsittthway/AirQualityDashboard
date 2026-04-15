8a53c6e09a196788e6cddfc1f14789bd19bf2b08
19383273-Mayowa-Adebambo <19383273@brookes.ac.uk>
2026-04-15 16:48:13 +0100

    removed toggles in wrong pages

--- src/callbacks.py
+++ src/callbacks.py
@@ -2848,13 +2848,15 @@ def register_callbacks(app, wales_df, wales_df_long):
         forecast_site_style = {'display': 'none'}
         threshold_toggle = {'display': 'block'}
         quality_toggle = {'display': 'block'}
-
-        if pathname == '/exceedance':
+        if pathname == '/':
+            threshold_toggle = {'display': 'none'}
+        elif pathname == '/comparison':
+            threshold_toggle = {'display': 'none'}
+        elif pathname == '/exceedance':
             date_style = {'display': 'none'}
             year_style = {'display': 'block'}
 
-        elif pathname == '/forecast':  # Or your specific forecast URL
-            # Hide the Multi-select, Show the Single-select
+        elif pathname == '/forecast':
             date_style = {'display': 'none'}
             main_site_style = {'display': 'none'}
             forecast_site_style = {'display': 'block'}
